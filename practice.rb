fe92997451a599d0bf96ae70d98d5a6f86c0313a
lkpallarca <Ckencepallarca@gmail.com>
2022-05-14 12:21:09 +0800

    accomplish activity 5

--- practice.rb
+++ practice.rb
@@ -1,10 +1,71 @@
-class Car
-  attr_reader :brand, :model, :color
+class Employee
+  attr_accessor :sex, :age, :rating, :status, :base_salary
 
-  def initialize(brand, model, color)
-    @brand = brand
-    @model = model
-    @color = color
-    @engine_status = :off
+  def initialize(sex, age, base_salary)
+    @sex = sex
+    @age = age
+    @base_salary = base_salary
+    @status = true #employed
+    @rating = nil #1 - 5
+  end
+
+  def resign
+    @status = false
+  end
+
+  def resigned?
+    !@status
+  end
+end
+
+module OvertimePayable
+  def calculate_overtime_pay
+    base_salary * 2
+  end
+end
+
+class Accountant < Employee
+  include OvertimePayable
+
+  def audit
+    puts "Auditing..."
+  end
+end
+
+class SoftwareEngineer < Employee
+  include OvertimePayable
+
+  def write_code
+    puts "Lorem Ipsum..."
+  end
+end
+
+class BackEndEngineer < SoftwareEngineer
+  def write_code
+    puts "Backend code..."
+  end
+end
+
+class FrontEndEngineer < SoftwareEngineer
+  def write_code
+    puts "Frontend code..."
+  end
+end
+
+class Manager < Employee
+  def evaluate_performance(employee, score)
+    employee.rating = score
+  end
+end
+
+class CTO < Employee
+  def initialize(age, sex, equity)
+    super(age, sex)
+    @equity = equity
+  end
+
+  def resign
+    puts "Some additional paperwork..."
+    super
   end
 end
