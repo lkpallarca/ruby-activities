e78b5a70b1c111b4f390493cc411bfa6fc90892e
lkpallarca <Ckencepallarca@gmail.com>
2022-05-19 20:29:13 +0800

    refactor to DRY code

--- db_activity_1.sql
+++ db_activity_1.sql
@@ -10,31 +10,21 @@ CREATE TABLE students (
 );
 
 INSERT INTO students (id, first_name, middle_name, last_name, age, location, created_at, updated_at)
-VALUES (1, 'Juan', 'Blank', 'Cruz', 18, 'Manila', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES 
+  (1, 'Juan', 'Blank', 'Cruz', 18, 'Manila', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (2, 'Lance Kennard', 'Mahigne', 'Pallarca', 24, 'Parañaque', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (3, 'Shalyn', 'Pagunsan', 'Gallano', 25, 'Parañaque', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (4, 'Patrick', 'Blank', 'Samson', 24, 'Cavite', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (5, 'Fey Daniel', 'Blank', 'Sy', 24, 'Bulacan', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(6, 'Jomyrrjeff', 'Blank', 'Tabigne', 24, 'Parañaque', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location, created_at, updated_at)
-VALUES (2, 'Lance Kennard', 'Mahigne', 'Pallarca', 24, 'Parañaque', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO students (id, first_name, middle_name, last_name, age, location, created_at, updated_at)
-VALUES (3, 'Shalyn', 'Pagunsan', 'Gallano', 25, 'Parañaque', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO students (id, first_name, middle_name, last_name, age, location, created_at, updated_at)
-VALUES (4, 'Patrick', 'Blank', 'Samson', 24, 'Cavite', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO students (id, first_name, middle_name, last_name, age, location, created_at, updated_at)
-VALUES (5, 'Fey Daniel', 'Blank', 'Sy', 24, 'Bulacan', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO students (id, first_name, middle_name, last_name, age, location, created_at, updated_at)
-VALUES (6, 'Jomyrrjeff', 'Blank', 'Tabigne', 24, 'Parañaque', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-UPDATE students SET first_name = 'Ivan' WHERE id = 1;
-UPDATE students SET middle_name = 'Ingram' WHERE id = 1;
-UPDATE students SET last_name = 'Howard' WHERE id = 1;
-UPDATE students SET age = 25 WHERE id = 1;
-UPDATE students SET location = 'Bulacan' WHERE id = 1;
+UPDATE students SET 
+  first_name = 'Ivan' WHERE id = 1,
+  middle_name = 'Ingram' WHERE id = 1,
+  last_name = 'Howard' WHERE id = 1,
+  age = 25 WHERE id = 1,
+location = 'Bulacan' WHERE id = 1;
 
 SELECT * FROM students WHERE id = 1;
 
 DELETE FROM students WHERE id = 6;
-
--- insert into can be shortened
